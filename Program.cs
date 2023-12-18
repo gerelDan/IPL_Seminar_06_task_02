0ab0c45d40f9025eb1e447138bcdbf90cccafd14
gerel <gerel@bk.ru>
2023-12-18 16:51:34 +0500

    Add Task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Задайте строку, содержащую латинские буквы
+//в обоих регистрах. Сформируйте строку,
+//в которой все заглавные буквы заменены на строчные.
